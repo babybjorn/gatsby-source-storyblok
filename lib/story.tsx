aadc92460217e6629adc4229175daf8c18ceb529
Rasmus Graham <2124386+rasmusbe@users.noreply.github.com>
2025-07-04 14:35:29 +0200

    fix: type errors

--- lib/story.tsx
+++ lib/story.tsx
@@ -1,21 +1,25 @@
 "use client";
 
-import React, { forwardRef } from "react";
 import { StoryblokComponent } from "@storyblok/react";
+import React, { forwardRef } from "react";
 import { useStoryblokState } from "./src/common";
 import { SbGatsbyStory, StoryblokBridgeConfigV2 } from "./types";
 
 interface StoryblokStoryProps {
   story: SbGatsbyStory;
   bridgeOptions: StoryblokBridgeConfigV2;
-  [key: string]: unknown;
 }
 
-const StoryblokStory = forwardRef<HTMLElement, StoryblokStoryProps>(
-  ({ story, bridgeOptions, ...restProps }, ref) => {
-    story = useStoryblokState(story, bridgeOptions);
-    return <StoryblokComponent ref={ref} blok={story.content} {...restProps} />;
-  }
-);
+const StoryblokStory = forwardRef<
+  HTMLElement,
+  StoryblokStoryProps & Record<string, unknown>
+>((props, ref) => {
+  const { story, bridgeOptions, ...restProps } = props as StoryblokStoryProps &
+    Record<string, unknown>;
+  const updatedStory = useStoryblokState(story, bridgeOptions);
+  return (
+    <StoryblokComponent ref={ref} blok={updatedStory.content} {...restProps} />
+  );
+});
 
 export default StoryblokStory;
